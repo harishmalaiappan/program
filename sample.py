bb9f63d20b982e675625241a7af8935ac753a293
unknown <you@example.com>
2019-02-01 20:39:07 +0530

    initial stage

---
+++ sample.py
@@ -0,0 +1,62 @@
+var=[2,4,9]
+var1=[9,10,1]
+print(var+var1)
+var="onion"
+for x in var:
+    if x=='o':
+        print(x)
+var="dhoni"
+print(var[1])
+var="dhoni"
+print(var[1:])
+var="dhoni"
+print(var[1:3])
+var="dhoni"
+print(var[:3])
+var="dhoni"
+print(var[2:2])
+var="dhoni"
+print(var[1:-2])
+var="dhoni"
+print(var[1:-1]
+var="dhoni"
+print(var[-2:-1])
+var="dhoni is my"
+print(var[::2])
+var="dhoni is my"
+print(var[::-1])
+var="DHONI"
+for x in var:
+    print(x)
+var="DHONI"
+for x in var:
+    if x=='N':
+        print("avail")
+var="DHONINSN"
+for x in var:
+    if x=='N':
+        print("avail")
+var="DHONINSN"
+for x in var:
+    if x=='N':
+        print("avail")
+        break
+var="DHONINSN"
+for x in enumerate(var):
+    print(x)
+var="DHONINSN"
+for x in enumerate(var):
+    if x=='N':
+        print(x)
+var="DHONINSN"
+for x,y in enumerate(var):
+    if(y=='N'):
+        print(y)
+var="DHONINSN"
+for x,y in enumerate(var):
+    if(y=='N'):
+        print(y)
+var="c\new\text"
+print(var)
+var=r"c:\new\text"
+print(var)
